502e36c9dd29e622e491023d33f63d506a0410d4
Noah <noahtullysanderson@gmail.com>
2018-01-20 22:29:24 -0500

    finished coding

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,61 +1,212 @@
-$(document).ready(function(){
-  $("#start-button").click(function(){
-// ----------------------------------------------------------------
- // declaring the value for the timer to 60 seconds
- // hide the start button and rules
-  	var number = 50;
-  	alert("The let game begin!");
-    $("#start-button").on("click", start);  // starts the games 
-    $("#submit").on("click", finish);  // submits answers and finishes the game
-    $("#restart").on("click", restart);  // restarts the games 
-// ----------------------------------------------------------------
-// functions
-    function start(){
-    	counter = setInterval(timer, 1000);
-    	showMe(".question");
-    	showMe(".answers");
-    	showMe("#submit");
-	    hideMe("#start-button");
-	    hideMe(".rules");
-	    hideMe("#restart");
-	    hideMe("#results");
-    }
-    function timer(){
-      number-- // decrements the timer by 1
-      $("#show-number").html("<h2>" + number + "</h2>" );
-      if (number === 0){
-        alert("Times Up!")
-        stop(); // calls the stop function
-      }
-    }
-    function stop(){
-    	clearInterval(counter); // stops the timer
-    	$("#results").show();
-    	$("#restart").show();
-		$(".question").hide();
-		$(".answers").hide();
-		$("#submit").hide();
-    }
-    function finish(){
-    	number = 1; // if number is equal to 0 number will show -1 so 1 has to be selected
-    	clearInterval(counter); // stops the timer
-    	timer();
+$(document).ready(function() {
+    $("#introSection").hide();
+    $("#messageSection").hide();
+    $('#instructionModal').modal();
+    $('.parallax').parallax(); // function for MaterializeCSS parallax componenet
+    $('.tooltipped').tooltip({ // delay function for button tool tips
+        delay: 50
+    });
+
+
+    $("#introSection").fadeIn(1000 * 3, function() { // fade in page elements
+        // fadeIn function
+    });
+
+    $("#questionSpace").hide()
+    var correctCounter = 0,
+        incorrectCounter = 0,
+        unansweredCounter = 0,
+        currentQuestionIndex = 0;
+
+
+    var congratsMessages = ['good job ', 'Wow you know alot about NYC', "Amazing Knowledge!"];
+
+    function randomNum(x) {
+        var roll = Math.floor(Math.random() * x);
+        return roll;
     }
 
-    function restart(){
-    	number = 50;
-    	start();
+    function randomCongrats() {
+        var messageRoll = randomNum(congratsMessages.length);
     }
 
-    function hideMe(e) {
-    	$(e).hide();
-    }
-    function showMe(e) {
-    	$(e).show();
+    function countDown() {
+        $('.pickAnswer').click(function() {
+            $(this).data('clicked', true);
+        });
+        var i = 30;
+        var myInterval = setInterval(function() {
+
+            if (i < 10) {
+                $('#timerSeconds').html("0" + i);
+                $(".pickAnswer").on("click", function() {
+                    clearInterval(myInterval);
+                })
+            } else {
+                $('#timerSeconds').html(i);
+                $(".pickAnswer").on("click", function() {
+                    clearInterval(myInterval);
+                })
+            }
+
+            if (i === 0) {
+                unansweredCounter++;
+                clearInterval(myInterval);
+                currentQuestionIndex++;
+                $('#timer').effect("pulsate", {
+                    times: 25
+                }, 1000 * 5);
+                i = 30;
+                postQuestion(currentQuestionIndex);
+            } else {
+                i--;
+            }
+        }, 1000);
     }
 
-// ----------------------------------------------------------------
-//calling functions
-  	start(); // calls the start function
-  });
+    var questions = [
+        // question 1
+        {
+            "q": "How big is New York?",
+            "c": ["407 square miles", "390 square miles", "305 square miles"],
+            "answer": 2
+        },
+        // question 2
+        {
+            "q": "How many people live in New York City?",
+            "c": ["11.6 million people", "8.4 million people", "7.2 million people"],
+            "answer": 1
+        },
+        // question 3
+        {
+            "q": "How big is The Empire State Building (to the top floor)?",
+            "c": ["1450 feet", "1365 feet", "1250 feet"],
+            "answer": 2
+        },
+        // question 4
+        {
+            "q": " How tall is the Chrysler Building?",
+            "c": ["1046 feet", "992 feet", "967 feet"],
+            "answer": 0
+        },
+        // question 5
+        {
+            "q": "Whos the richest person living in New York?",
+            "c": [" Michael Bloomberg", "David Koch", "Carl Icahn"],
+            "answer": 0
+        },
+        // question 6
+        {
+            "q": "Whats the biggest park in New York?",
+            "c": ["Bronx Park", "Pelham bay park", "Central Park"],
+            "answer": 1
+        },
+        // question 7
+        {
+            "q": "How long is long beach?",
+            "c": ["3 1/2 miles", "2 1/4 miles", "1 2/3 miles long"],
+            "answer": 1
+        },
+        // question 8
+        {
+            "q": "What the largest borough in New York?",
+            "c": ["Brooklyn", "Manhatten", "Queens"],
+            "answer": 2
+        },
+        // question 9
+        {
+            "q": "How tall is the 432 park avenue building?",
+            "c": ["1396 feet", "1230 feet", "1145 feet"],
+            "answer": 0
+        },
+        // question 10
+        {
+            "q": "When was New York City established",
+            "c": ["1610", "1624", "1654"],
+            "answer": 1
+        }
+    ];
+
+
+    function postQuestion(n) {
+
+        if (currentQuestionIndex < questions.length) {
+            $('#question').remove();
+            $('.pickAnswer').remove();
+            countDown();
+            $('#questionContainer').append("<div id='question'>" + questions[n].q + "</div>");
+            for (var i = 0; i < questions[n].c.length; i++) {
+                var newDiv = $("<div>");
+                newDiv.addClass("pickAnswer").attr("indexnum", i).text(questions[n].c[i]);
+                $('#choices').append(newDiv);
+            }
+
+
+        } else {
+            resetGame(); // the conditional successfully loops the game
+        }
+
+        $(".pickAnswer").on("click", function() {
+            var userChoice = $(this).attr('indexnum'); // stored as a string not a number
+            userChoice = parseInt(userChoice);
+
+            // checks if user is correct and will tally accordingly
+            if (userChoice === questions[currentQuestionIndex].answer) {
+                correctCounter++;
+                currentQuestionIndex++
+                randomCongrats();
+
+            } else {
+                incorrectCounter++;
+                currentQuestionIndex++;
+
+            }
+            postQuestion(currentQuestionIndex);
+        })
+    }
+
+    function startTrivia() {
+        $('#messageSection').hide();
+        $('#gameMessage').empty()
+        $('#questionContainer').show();
+        $('#choices').show();
+        $("#timer").show();
+        correctCounter = 0;
+        incorrectCounter = 0;
+        unansweredCounter = 0;
+        currentQuestionIndex = 0;
+
+        postQuestion(currentQuestionIndex);
+
+    }
+
+    function resetGame() {
+        $('#messageSection').show();
+        $('#questionContainer').hide();
+        $('#choices').hide();
+        $('#timer').hide()
+
+        $('#gameMessage').append("<h2>You have completed the game!</h2>");
+        $('#gameMessage').append("<h4>Total Correct: " + correctCounter + "</h4>");
+        $('#gameMessage').append("<h4>Total Incorrect: " + incorrectCounter + "</h4>");
+        $('#gameMessage').append("<h4>Total Unanswered: " + unansweredCounter + "</h4>");
+
+        setTimeout(startTrivia, 1000 * 10);
+
+    }
+
+
+
+    $("#startButton").on("click", function() {
+        $("#buttonRow").hide();
+        $("#introCard").remove();
+        $("#timer").append("<span id='timerMinutes'>00</span>:<span id='timerSeconds'>00</span>");
+        $("#questionSpace").show();
+
+        startTrivia();
+
+
+    })
+
+
 });
